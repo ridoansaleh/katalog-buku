7bbfac5748a2fc5f51568b5e2f2a5b412f351f12
ridoansaleh <ridoansalehnasution98@gmail.com>
2019-08-17 21:25:37 +0700

    show book list

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,63 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import '../css/App.css';
+import { API_BASE_URL } from '../constant';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchKeyword: '',
+      books: [],
+    };
+
+    this.handleKeywordChange = this.handleKeywordChange.bind(this);
+  }
+
+  componentDidMount() {
+    axios
+      .get(API_BASE_URL + '/books')
+      .then(res => {
+        console.log(res);
+        this.setState({
+          books: res.data,
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  handleKeywordChange(e) {
+    this.setState({
+      searchKeyword: e.target.value,
+    });
+  }
+
   render() {
+    const { searchKeyword, books } = this.state;
     return (
       <div>
-        <p>Hello World</p>
+        <input type="text" value={searchKeyword} onChange={this.handleKeywordChange} />
+        <section className="books-container">
+          {books.map(book => {
+            return (
+              <div key={book.id}>
+                <img src={book.cover} className="book-cover" />
+                <p className="book-title">{book.title}</p>
+                <div className="book-value-wrapper">
+                  <p>{book.price}</p>
+                  <p>Rating {book.rating}</p>
+                </div>
+                <p>{book.author}</p>
+                <button type="button" className="book-show-btn">
+                  Lihat
+                </button>
+              </div>
+            );
+          })}
+        </section>
       </div>
     );
   }
