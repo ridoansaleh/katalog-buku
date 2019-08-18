aa162e923f2d3d2a7e7a331736a1d237d2e83f31
RSN <ridoansalehnasution98@gmail.com>
2019-08-18 12:58:08 +0700

    add home and book detail page (#1)
    * show book list
    
    * add book detail page

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,93 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 import '../css/App.css';
+import { API_BASE_URL } from '../constant';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchKeyword: '',
+      books: [],
+      listOfBook: [],
+    };
+
+    this.handleKeywordChange = this.handleKeywordChange.bind(this);
+  }
+
+  componentDidMount() {
+    axios
+      .get(API_BASE_URL + '/books')
+      .then(res => {
+        this.setState({
+          books: res.data,
+          listOfBook: res.data,
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  handleKeywordChange(e) {
+    const { listOfBook } = this.state;
+    const val = e.target.value;
+
+    this.setState(
+      {
+        searchKeyword: val,
+      },
+      () => {
+        const result = listOfBook.filter(book => {
+          if (book.title.startsWith(val[0]) || book.title.search(val) > -1) {
+            return book;
+          }
+        });
+        this.setState({
+          books: result,
+        });
+      },
+    );
+  }
+
   render() {
+    const { searchKeyword, books } = this.state;
     return (
       <div>
-        <p>Hello World</p>
+        <div className="navbar">
+          <span className="navbar-title">Toko Buku</span>
+        </div>
+        <section className="search-box">
+          <input
+            type="text"
+            className="search-input"
+            value={searchKeyword}
+            onChange={this.handleKeywordChange}
+            placeholder="Cari nama buku"
+          />
+        </section>
+        <section className="books-container">
+          {books.map(book => {
+            return (
+              <div key={book.id}>
+                <img src={book.cover} className="book-cover" />
+                <p className="book-title">{book.title}</p>
+                <div className="book-value-wrapper">
+                  <p>{book.price}</p>
+                  <p>Rating {book.rating}</p>
+                </div>
+                <p>{book.author}</p>
+                <Link to={`/book/${book.id}`}>
+                  <button type="button" className="book-show-btn">
+                    Lihat
+                  </button>
+                </Link>
+              </div>
+            );
+          })}
+        </section>
       </div>
     );
   }
---
+++ src/constant.js
@@ -0,0 +1,1 @@
+export const API_BASE_URL = 'https://my-json-server.typicode.com/ridoansaleh/books-api';
